71e2b3bac92066bd2a53f0b4f25234dc2b1e49bb
Ana20006 <78221601+Ana20006@users.noreply.github.com>
2026-04-22 00:51:45 +0000

    Completed register page with navigation and full flow

--- homeneeds/services/views.py
+++ homeneeds/services/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'services/home.html')
 
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if email and password:
+            return redirect('dashboard')
+
+    return render(request, 'services/login.html')
+
+def register_view(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if first_name and email and password:
+            return redirect('login')
+
+    return render(request, 'services/register.html')
+
+def dashboard(request):
+    return render(request, 'services/dashboard.html')
