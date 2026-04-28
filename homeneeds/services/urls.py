3ba5fa76a12a20b5e1e9ce95105e6937cf6821d6
Ana20006 <78221601+Ana20006@users.noreply.github.com>
2026-04-28 21:37:51 +0000

    Save before fixing merge

--- homeneeds/services/urls.py
+++ homeneeds/services/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- homeneeds/services/views.py
+++ homeneeds/services/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 
 def home(request):
     return render(request, 'services/home.html')
 
 def login_view(request):
+    error = None
+
     if request.method == "POST":
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        if email and password:
-            return redirect('dashboard')
+        user = authenticate(request, username=email, password=password)
 
-    return render(request, 'services/login.html')
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            error = "Invalid email or password"
+
+    return render(request, 'services/login.html', {'error': error})
 
 def register_view(request):
     if request.method == "POST":
@@ -20,9 +32,22 @@ def register_view(request):
         password = request.POST.get('password')
 
         if first_name and email and password:
+            User.objects.create_user(
+                username=email,
+                email=email,
+                password=password
+            )
             return redirect('login')
 
     return render(request, 'services/register.html')
 
 def dashboard(request):
     return render(request, 'services/dashboard.html')
+
+@login_required
+def dashboard(request):
+    return render(request, 'services/dashboard.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
